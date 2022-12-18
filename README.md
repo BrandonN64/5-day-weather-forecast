3292e9700a613742aa42343b464c309c69657ce3
BrandonN64 <Brandon.nastri@gmail.com>
2022-12-18 15:17:35 -0500

    added files that are required to the repo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # module-6-readme-generator
+
+For this homework project, we were tasked with making a 5 day weather forecast app using HTML, Javascript, and a 3rd party API called OpenWeather. 
+
+With this app, you would enter a city name and it would make an API call to get the 5 day weather forecast for the specified area.
+
---
+++ assets/index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+var searchButton = document.getElementById("searchButton")
+var userCity = document.querySelector('#cityName')
+var openWeatherApi = '5d21e5ba4232f7c4c356ba55ff9657a1'
+var apiCallUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity.value + "&appid=" + openWeatherApi
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
